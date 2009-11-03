65d3d8c6e6b1b21f90638cd7f08a275753ba2460
Lloyd Hilaiel <lloyd@hilaiel.com>
2009-11-03 16:12:39 -0700

    output to 'built' directory

--- external/build.rb
+++ external/build.rb
@@ -3,7 +3,7 @@
 require "bakery/ports/bakery"
 
 $order = {
-  :output_dir => File.join(File.dirname(__FILE__), "ext"),
+  :output_dir => File.join(File.dirname(__FILE__), "built"),
   :packages => [ "zlib", "libpng", "jpeg", "GraphicsMagick", ],
   :verbose => true
 }
