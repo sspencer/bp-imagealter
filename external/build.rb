9b8c040e8e117128b66658a83fa39ffc8014badd
Lloyd Hilaiel <lloyd@hilaiel.com>
2009-11-03 16:07:56 -0700

    a buildscript guiding the bakery

---
+++ external/build.rb
@@ -0,0 +1,12 @@
+#!/usr/bin/env ruby
+
+require "bakery/ports/bakery"
+
+$order = {
+  :output_dir => File.join(File.dirname(__FILE__), "ext"),
+  :packages => [ "zlib", "libpng", "jpeg", "GraphicsMagick", ],
+  :verbose => true
+}
+
+b = Bakery.new $order
+b.build
