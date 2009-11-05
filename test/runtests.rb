044bb6c1a6f334baed04eb336ad1597a4e8a0a71
Lloyd Hilaiel <lloyd@hilaiel.com>
2009-11-05 10:59:38 -0700

    complete and working test harness.  w00t

--- test/runtests.rb
+++ test/runtests.rb
@@ -1,5 +1,7 @@
 #!/usr/bin/env ruby
 
+require 'uri'
+
 # if we're talkin' ruby 1.9, we'll use built in json, otherwise use
 # the pure ruby library sittin' here
 $:.push(File.dirname(__FILE__))
@@ -18,28 +20,32 @@ clet = File.join(File.dirname(__FILE__), "..", "src", "build", "ImageAlter")
 raise "can't execute ServiceRunner: #{sr}" if !File.executable? sr
 raise "can't find built service to test: #{clet}" if !File.directory? clet
 
-def mypread(pio, timeo)
+def mypread(pio, timeo, returnAfterInput = false)
   output = String.new
-  puts "selecting on #{pio}"
   while nil != select( [ pio ], nil, nil, timeo )  
-    puts "selecting got data!"
     output += pio.sysread(1024) 
+    break if output.length && returnAfterInput 
   end
-  puts "returning #{output.length} bytes (#{output})"
   output
 end
 
-IO.popen("#{sr} -log debug #{clet}", "w+") do |srp|
+rv = 0
+
+IO.popen("#{sr} #{clet}", "w+") do |srp|
   puts "spawned ServiceRunner"
   # discard startup stuff
-  mypread(srp, 0.5)
+  mypread(srp, 0.5, true)
   puts "allocating instance"
   srp.syswrite "allocate\n"
-  mypread(srp, 0.5)
+  mypread(srp, 0.5, true)
+
+  tests = 0
+  successes = 0
 
   # now let's iterate through all of our tests
   Dir.glob(File.join(File.dirname(__FILE__), "cases", "*.json")).each do |f|
-    puts "-- #{f} -- "
+    tests += 1 
+    $stdout.write "#{File.basename(f, ".json")}: "
     json = JSON.parse(File.read(f))
     # now let's change the 'file' param to a absolute URI
     p = File.join(File.dirname(__FILE__), "test_images", json["file"])
@@ -47,11 +53,48 @@ IO.popen("#{sr} -log debug #{clet}", "w+") do |srp|
     # now convert p into a file url
     json["file"] = "file://" + p
     cmd = JSON.generate(json).gsub("'", "\\'")
-    cmd = "inv transform '#{cmd}'\n"
-    x = srp.syswrite cmd
-    puts "wrote #{x} of #{cmd.length} [#{cmd}]"
-    rez = mypread(srp, 5.0)
-    puts rez
+    # NOTE: the appended "show" command.  Service runner as of
+    #       2.4.20 has a bug where it doesn't flush output.  So the
+    #       results of invocation can be stuck in service runner's
+    #       output buffer.  The 'show' command causes a flush.
+    #       fix committed for 2.5.x branch, and we can remove this
+    #       once that's live 
+    srp.syswrite "inv transform '#{cmd}'\nshow\n"
+    rez = mypread(srp, 1.0, true)
+
+    # now rez is of the form >{ "file": "file:///foo.x" } 1 instance...<
+    # we'll strip off the trailing gunk, extract the file url
+    # and compare it to the .out file.  if we faqil anywhere along this
+    # path, then we'll give up and call it a failure
+    imgGot = nil
+    begin
+      robj = JSON.parse(rez.sub(/\}.*$/m, '}'))
+      gotImgPath = URI.parse(robj['file']).path
+      imgGot = File.open(gotImgPath, "rb") { |oi| oi.read }
+      wantImgPath = File.join(File.dirname(f),
+                              File.basename(f, ".json") + ".out")
+      raise "no output file for test!" if !File.exist? wantImgPath
+      imgWant = File.open(wantImgPath, "rb") { |oi| oi.read }
+      raise "output mismatch" if imgGot != imgWant
+      # yay!  it worked!
+      successes += 1
+      puts "ok."
+    rescue => e
+      err = e.to_s
+      # for convenience, if the test fails, we'll *save* the output
+      # image in xxx.got
+      if imgGot != nil
+        gotPath = File.join(File.dirname(f),
+                            File.basename(f, ".json") + ".got")
+        File.open(gotPath, "wb") { |oi| oi.write(imgGot) }
+        err += " [left result in #{File.basename(gotPath)}]"
+      end
+      puts "fail (#{err})"
+    end
   end
-  puts "done!"
+  puts "#{successes}/#{tests} tests completed successfully"
+  
+  rv = successes == tests
 end
+
+exit rv
